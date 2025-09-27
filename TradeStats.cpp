27bea15d9442c30d875d7e48ff27bbe52395be75
Jeffrey Litvin <litvin.j@gmail.com>
2025-09-27 12:52:55 -0400

    graph r

---
+++ TradeStats.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include "Trade.hpp"
+#include "TradeStats.hpp"
+#include <cxxopts/cxxopts.hpp>
+#include "csv.h"
+#include "ascii/ascii.h"
+
+int main(int argc, char *argv[])
+{
+    cxxopts::Options options("TradeStats", "Produces trade stats");
+    options.add_options()
+        ("f,file", "File name", cxxopts::value<std::string>())
+        ("h,help", "Print usage")
+        ;
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help"))
+    {
+      std::cout << options.help() << std::endl;
+      exit(0);
+    }
+    std::string file;
+    if (result.count("file"))
+    {
+        file = result["file"].as<std::string>();
+    }
+    else
+    {
+        std::cout << "File argument required!" << std::endl;
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+
+    TradeStatManager mgr;
+    mgr.readTradeFile(file);
+
+    const auto& stats = mgr.getTradeStats("ALL");
+    std::cout << stats.getLabel() << " total_trades=" << stats.getTotalTrades()
+                                  << " win_pct=" <<  std::fixed << std::setprecision(2) << stats.getWinPct()
+                                  << " total_r=" << stats.getRunningR()
+                                  << std::endl;
+    
+    ascii::Asciichart asciichart({{"ALLTRADEs", mgr.getTradeStats("ALL").getTradesAsSeries()}});
+    std::cout << asciichart.Plot() << std::endl;
+
+}
---
+++ TradeStats.hpp
@@ -0,0 +1,79 @@
+ #pragma once
+
+#include <string>
+#include <fstream>
+#include "csv.h"
+#include "Trade.hpp"
+
+class TradeStats
+{
+    public:
+
+    TradeStats(std::string label) : _label(label)
+    { }
+
+    void addTrade(double r)
+    {
+        if(r >= 0)
+        {
+            ++_wins;
+        }
+
+        _runningR += r;
+        _tradeR.push_back(_runningR);
+    }
+
+    std::string getLabel() const  { return _label;}
+    int getTotalTrades()   const  { return _tradeR.size();}
+    double getRunningR()      const  { return _runningR;}
+    int getWins()          const  { return _wins;}
+    double getWinPct() const
+    {
+        return (static_cast<double>(getWins()) / static_cast<double>(getTotalTrades())) * 100;  
+    }
+
+    const std::vector<double>& getTradesAsSeries() const { return _tradeR;};
+
+    private:
+    std::string _label;
+    std::vector<double> _tradeR;
+    int _wins        = 0;
+    double _runningR    = 0;
+
+};
+
+class TradeStatManager
+{
+    public:
+
+    void readTradeFile(std::string f)
+    {
+        io::CSVReader<9> in(f);
+
+        int longTrade;
+        int withTrend;
+        double r;
+        Trade t;
+
+        while (in.read_row(t.sym, t.openDate, longTrade, withTrend, t.tradeType, t.risk, t.closeDate, t.pnl, r))
+        {
+            t.longTrade = longTrade;
+            t.withTrend = withTrend;
+            addTrade(t);
+        }
+    }
+
+    void addTrade(Trade t)
+    {
+        double r = static_cast<double>(t.pnl) / static_cast<double>(t.risk);
+        _allTrades.addTrade(r);
+    }
+
+    const TradeStats& getTradeStats(const std::string& label)
+    {
+        return _allTrades;
+    }
+
+    private:
+    TradeStats _allTrades{"ALL"};
+};
